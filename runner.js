2767f731362617bd47d58a124d933df6dee95343
Benjamin Singer <benjaminjsinger@gmail.com>
2016-06-10 18:27:29 -0500

    now a dope command runner

--- runner.js
+++ runner.js
@@ -1,61 +1,94 @@
 var cp = require('child_process');
+var untildify = require('untildify');
+var fs = require('fs');
 
-module.exports = function(){
-  return {
-    run: function(cmdObjIn, ws, timeout){
-      var self = this;
-      var cmdObj = JSON.parse(cmdObjIn);
-      var spawn = cp.spawn;
-      var error = false;
-      
-      try {
-        process.chdir(cmdObj.path);     
-      }catch(err){
-        console.log("Error setting command working directory...");
-        console.log(err);
-        error = true;
-        ws.send("Error setting command working directory...");
-      }
+module.exports = function() {
+    var currentDir = '~';
+    var spawn = cp.spawn;
+    var error = false;
+    var command;
+    var randomHex;
+    return {
+        // parseCommands: function(cmd) {
+        //     //replace all spaces
+        //     cmd = cmd.replace(/\s{2,}/, ' ');
+        // },
+        run: function(cmdObjIn, ws, timeout) {
+            var self = this;
+            var cmdObj = JSON.parse(cmdObjIn);
 
-      if(!error){
-        try{
-          var cmdArray = cmdObj.command.split(' ')
-          var cmd = cmdArray.splice(0, 1)[0];
-          var command = spawn(cmd, cmdArray);
 
-          if(timeout && typeof timeout === 'number'){
-            self.timeoutId = setTimeout(function(){
-              command.kill('SIGSTOP');
-              ws.send("Command timeout reached...");
-            }, timeout * 1000);
-          }
+            console.log(currentDir);
+            try {
+                process.chdir(untildify(currentDir));
+            } catch (err) {
+                console.log("Error setting command working directory...");
+                console.log(err);
+                error = true;
+                ws.send("Error setting command working directory...");
+            }
 
-          command.stdout.on('data', function (data) {
-            data = String(data).replace(/(?:\n\r?|\r\n?)/g, '<br>').replace(/(\s)|(\[K)/g, '&nbsp;');
-            ws.send(data);
-          });
+            if (!error) {
+                try {
+                    // var cmdArray = cmdObj.command.split(' ');
+                    // var cmd = cmdArray.splice(0, 1)[0];
+                    randomHex = '#'+Math.floor(Math.random()*16777215).toString(16);
+                    command = cmdObj.command.trim();
+                    if(command.length > 0){
+                        if(command[command.length -1] === ';'){
+                            command = command.substring(0, command.length - 1);
+                        }
+                        command = command + '; echo "' + randomHex + '`pwd`"';
+                        fs.writeFile(untildify("~/.tmpScript.sh"), command, function(err) {
+                            if (err) {
+                                return console.log(1, err);
+                            }
+                            var proc = spawn(untildify("~/.tmpScript.sh"));
 
-          command.stderr.on('data', function (data) {
-            data = "<span class=\"error\">" + String(data).replace(/(?:\n\r?|\r\n?)/, '<br>') + "</span>";
-            ws.send(data);
-          });
+                            if (timeout && typeof timeout === 'number') {
+                                self.timeoutId = setTimeout(function() {
+                                    command.kill('SIGSTOP');
+                                    ws.send("Command timeout reached...");
+                                }, timeout * 1000);
+                            }
 
-          command.on('exit', function (code) {
-            clearTimeout(self.timeoutId);
-            //console.log('child process exited with code ' + code);
-            ws.send('child process exited with code ' + code + '<br><br>');
-          });
-        }catch(err){
-          console.log("Error running command and returning output");
-          console.log(err);
-          error = true;
-          ws.send("Error running command and returning output...");
+                            proc.stdout.on('data', function(data) {
+                                data = String(data);
+                                var array = data.split(randomHex);
+                                // console.dir(array);
+                                // console.log(array.length);
+                                data = array[0];
+                                if(array.length > 1){
+                                    var pwd = array[array.length -1];
+                                    currentDir = untildify(pwd.trim());
+                                }
+                                data = '<br>' + data.replace(/(?:\n\r?|\r\n?)/g, '<br>').replace(/(\s)|(\[K)/g, '&nbsp;');
+                                ws.send(data);
+                            });
+
+                            proc.stderr.on('data', function(data) {
+                                data = "<span class=\"error\">" + String(data).replace(/(?:\n\r?|\r\n?)/, '<br>') + "</span><br>";
+                                ws.send(data);
+                            });
+
+                            proc.on('exit', function(code) {
+                                clearTimeout(self.timeoutId);
+                                //console.log('child process exited with code ' + code);
+                                // ws.send('child process exited with code ' + code + '<br><br>');
+                            });
+                        });
+                    }else{
+                        ws.send('');
+                    }
+                    // fs.unlink(untildify("~/.tmpScript.sh"), callback);
+
+                } catch (err) {
+                    console.log("Error running command and returning output");
+                    console.log(err);
+                    error = true;
+                    ws.send("Error running command and returning output...");
+                }
+            }
         }
-      }
-    }
-  }
-}
-
-
-
-  
+    };
+};
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var util= require('util');
+var util = require('util');
 var Runner = require('./runner');
 var fs = require('fs');
 var WebSocketServer = require('ws').Server;
@@ -7,72 +7,52 @@ var http = require('http');
 var Router = require('node-simple-router');
 var file = require('file');
 
-    
+
 d = domain.create();
 d.on('error', function(err) {
     console.error(err);
-})
+});
 
-var wss = new WebSocketServer({ port: 8081 });
+var wss = new WebSocketServer({
+    port: 8081
+});
+
+var runner = new Runner();
+
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    var runner = new Runner();
-    runner.run(message, ws, 60 * 20);
-  });
-  
-  ws.on('close', function(){
-    var i = wss.clients.indexOf(ws);
-    if(wss.clients.indexOf(ws) > -1){
-      delete wss.clients[i]; 
-    }
-  });
+    ws.on('message', function incoming(message) {
+        // console.log('received: %s', message);
+        runner.run(message, ws, 60 * 20);
+    });
 
-  ws.on('error', function(err){
-    console.log(err);  
-  });
+    ws.on('close', function() {
+        var i = wss.clients.indexOf(ws);
+        if (wss.clients.indexOf(ws) > -1) {
+            delete wss.clients[i];
+        }
+    });
+
+    ws.on('error', function(err) {
+        console.log(err);
+    });
 });
 
 var router = Router();
-fs.readFile('./index.html', function (err, html) {
-  if (err) {
-    throw err; 
-  }       
-  router.get("/", function (request, response) {
-    response.writeHeader(200, {"Content-Type": "text/html"});  
-    response.write(html);  
-    response.end();  
-  });
+fs.readFile('./index.html', function(err, html) {
+    if (err) {
+        throw err;
+    }
+    router.get("/", function(request, response) {
+        response.writeHeader(200, {
+            "Content-Type": "text/html"
+        });
+        response.write(html);
+        response.end();
+    });
 });
 
-router.post("/vagrant/search", function (request, response) {
-  var vagrantPaths = [];
-  file.walkSync(request.body.path, function(path, dirs, files){
-    if(files){
-      files.forEach(function(file){
-        if(file.indexOf('Vagrantfile') > -1){
-          vagrantPaths.push(path);
-        }
-      });
-    }
-  });
-
-  response.writeHeader(200, {"Content-Type": "application/json"});  
-  response.write(JSON.stringify({ paths: vagrantPaths }));  
-  response.end();  
-}); 
-
-router.post("/vagrant/up", function (request, response) {
-  
-
-  response.writeHeader(200);  
-  response.end();  
-}); 
- 
 var server = http.createServer(router);
 // Listen on port 8080 on localhost
 server.listen(8080, "localhost");
 
-console.log('server running on 8080')
-
-
+console.log('server running on 8080');
